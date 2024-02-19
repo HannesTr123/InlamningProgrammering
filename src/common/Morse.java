8fb13fb3d7b177ba689aa4b761751929ac635fa1
Hannes <hannes.tragardh@utb.ecutbildning.se>
2024-02-19 15:13:06 +0100

    testkommentar

--- src/common/Morse.java
+++ src/common/Morse.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Morse {
-
+//test kommentar
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
